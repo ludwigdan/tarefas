879105973e43d5a6928250fd0e9e0bcbf48e1118
141850 <141850@upf.br>
2020-09-11 20:21:47 -0300

    Banco de dados
    Ajuste no banco de dados.

--- database/database.sql
+++ database/database.sql
@@ -2,8 +2,8 @@
 -- Table grupo
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS grupo (
-  id_grupo INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_grupo SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   ds_grupo VARCHAR(30) NULL,
   PRIMARY KEY (id_grupo)
@@ -13,15 +13,15 @@ CREATE TABLE IF NOT EXISTS grupo (
 -- Table funcionario
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS funcionario (
-  id_funcionario INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_funcionario SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   nm_funcionario VARCHAR(50) NOT NULL,
   dt_nascimento DATE NULL,
   cpf VARCHAR(11) NULL,
   email VARCHAR(50) NULL,
   login VARCHAR(50) NOT NULL,
-  senha VARCHAR(100) NOT NULL,
+  senha TEXT NOT NULL,
   PRIMARY KEY (id_funcionario)
 );
 
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS funcionario (
 -- Table funcionario_grupo
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS funcionario_grupo (
-  id_funcionario_grupo INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_funcionario_grupo SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   id_funcionario INT NULL,
   id_grupo INT NULL,
@@ -51,8 +51,8 @@ CREATE TABLE IF NOT EXISTS funcionario_grupo (
 -- Table setor
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS setor (
-  id_setor INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_setor SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   ds_setor VARCHAR(30) NOT NULL,
   PRIMARY KEY (id_setor)
@@ -62,8 +62,8 @@ CREATE TABLE IF NOT EXISTS setor (
 -- Table tipo_tarefa
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS tipo_tarefa (
-  id_tipo_tarefa INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_tipo_tarefa SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   ds_tipo_tarefa VARCHAR(100) NOT NULL,
   id_setor INT NOT NULL,
@@ -79,8 +79,8 @@ CREATE TABLE IF NOT EXISTS tipo_tarefa (
 -- Table tipo_tarefa_func
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS tipo_tarefa_func (
-  id_tipo_tarefa_func INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_tipo_tarefa_func SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   id_tipo_tarefa INT NOT NULL,
   id_funcionario INT NOT NULL,
@@ -101,8 +101,8 @@ CREATE TABLE IF NOT EXISTS tipo_tarefa_func (
 -- Table tipo_tarefa_grupo
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS tipo_tarefa_grupo (
-  id_tipo_tarefa_func INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_tipo_tarefa_func SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   id_tipo_tarefa INT NOT NULL,
   id_grupo INT NOT NULL,
@@ -123,8 +123,8 @@ CREATE TABLE IF NOT EXISTS tipo_tarefa_grupo (
 -- Table aprovacao
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS aprovacao (
-  id_aprovacao INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_aprovacao SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   ds_aprovacao VARCHAR(100) NOT NULL,
   id_tipo_tarefa INT NOT NULL,
@@ -140,8 +140,8 @@ CREATE TABLE IF NOT EXISTS aprovacao (
 -- Table aprovacao_grupo
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS aprovacao_grupo (
-  id_aprovacao_grupo INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_aprovacao_grupo SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   id_grupo INT NOT NULL,
   id_aprovacao INT NOT NULL,
@@ -162,8 +162,8 @@ CREATE TABLE IF NOT EXISTS aprovacao_grupo (
 -- Table aprovacao_funcionario
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS aprovacao_funcionario (
-  id_aprovacao_func INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_aprovacao_func SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   id_funcionario INT NOT NULL,
   id_aprovacao INT NOT NULL,
@@ -184,8 +184,8 @@ CREATE TABLE IF NOT EXISTS aprovacao_funcionario (
 -- Table status
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS status (
-  id_status INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_status SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   ds_status VARCHAR(30) NOT NULL,
   PRIMARY KEY (id_status)
@@ -195,8 +195,8 @@ CREATE TABLE IF NOT EXISTS status (
 -- Table solicitacao
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS solicitacao (
-  id_solicitacao INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_solicitacao SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   titulo VARCHAR(100) NOT NULL,
   ds_solicitacao VARCHAR(1000) NOT NULL,
@@ -231,13 +231,13 @@ CREATE TABLE IF NOT EXISTS solicitacao (
 -- Table solicitacao_aprovacao
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS solicitacao_aprovacao (
-  id_status INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_solicitacao_aprovacao SERIAL NOT NULL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   id_aprovador INT NOT NULL,
   id_solicitacao INT NOT NULL,
   dt_aprovacao DATE NULL,
-  PRIMARY KEY (id_status),
+  PRIMARY KEY (id_solicitacao_aprovacao),
   CONSTRAINT fk_solicitacao_aprovacao_s
     FOREIGN KEY (id_solicitacao)
     REFERENCES solicitacao (id_solicitacao)
@@ -254,8 +254,8 @@ CREATE TABLE IF NOT EXISTS solicitacao_aprovacao (
 -- Table solicitacao_log
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS solicitacao_log (
-  id_solicitacao_log INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_solicitacao_log SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   log VARCHAR(100) NOT NULL,
   dt_log TIMESTAMP NOT NULL,
@@ -272,8 +272,8 @@ CREATE TABLE IF NOT EXISTS solicitacao_log (
 -- Table solicitacao_tarefa
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS solicitacao_tarefa (
-  id_solicitacao_tarefa INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_solicitacao_tarefa SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   ds_solicitacao_tarefa VARCHAR(30) NOT NULL,
   id_executante INT NULL,
@@ -303,8 +303,8 @@ CREATE TABLE IF NOT EXISTS solicitacao_tarefa (
 -- Table solicitacao_tarefa_anexo
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS solicitacao_tarefa_anexo (
-  id_solicitacao_tarefa_anexo INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_solicitacao_tarefa_anexo SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   id_solicitacao_tarefa INT NOT NULL,
   ds_arquivo VARCHAR(200) NOT NULL,
@@ -320,8 +320,8 @@ CREATE TABLE IF NOT EXISTS solicitacao_tarefa_anexo (
 -- Table solicitacao_tarefa_func
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS solicitacao_tarefa_func (
-  id_solicitacao_tarefa_func INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_solicitacao_tarefa_func SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   id_solicitacao_tarefa INT NOT NULL,
   id_funcionario INT NOT NULL,
@@ -342,8 +342,8 @@ CREATE TABLE IF NOT EXISTS solicitacao_tarefa_func (
 -- Table solicitacao_tarefa_grupo
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS solicitacao_tarefa_grupo (
-  id_solicitacao_tarefa_grupo INT NOT NULL,
-  dt_criacao TIMESTAMP NOT NULL,
+  id_solicitacao_tarefa_grupo SERIAL,
+  dt_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   dt_atualizacao TIMESTAMP NULL,
   id_solicitacao_tarefa INT NOT NULL,
   id_grupo INT NOT NULL,
@@ -359,3 +359,5 @@ CREATE TABLE IF NOT EXISTS solicitacao_tarefa_grupo (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
+
+INSERT INTO funcionario(nm_funcionario, login, senha) VALUES('Administrador', 'admin', '86174a1eda360ba9ed9774f30c7d684a6e3290d18862d6c6a25a7cf1739673e8633427119facdd87f9fe2220a83e8563188a90b2f5eb1127eefb54c1a1906320lL1PHjmIUTiGOw09an2mP84PjHL7G7Q8TE4ln4VK23g=');
